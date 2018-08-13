1941e46f67f243974c5ac5718649bb9cafe0b18f
Ben Emmons <ben@emmons.biz>
2018-08-13 16:45:07 +0100

    Added error handling for response

--- Lambda/Response.js
+++ Lambda/Response.js
@@ -1,17 +1,18 @@
 const Nexmo = require('nexmo');
 
-module.exports = {send: function(response, phoneNumber, error) {
-    var nexmo = new Nexmo({
-        apiKey: "c04da579", 
-        apiSecret: "6245e4a5cf7a9299"
+module.exports = {
+    send: function (errorStatus, response, phoneNumber) {
+        var nexmo = new Nexmo({
+            apiKey: "c04da579",
+            apiSecret: "6245e4a5cf7a9299"
+        });
+        if (errorStatus) {
+            nexmo.message.sendSms("447418342669", phoneNumber, "Query not valid.");
+            return ("Query not valid.")
+        } else {
+            nexmo.message.sendSms("447418342669", phoneNumber, response);
+            return ("Message sent successfully");
         }
-    );
-    if (error == null){    
-        nexmo.message.sendSms("447418342669", phoneNumber, response);
-        return("Message sent successfully");
-}
-    else{
-        nexmo.message.sendSms("447418342669", phoneNumber, "Query not valid.");
-        return("Query not valid.")
+
     }
-}};
+};
