ce3efa0e135ddbba292d306d1a405aff92970bda
Ben Emmons <ben@emmons.biz>
2018-08-13 14:26:38 +0100

    Just getting description from wikipedia rather than full page

--- Lambda/APIFunctions/Wikipedia.js
+++ Lambda/APIFunctions/Wikipedia.js
@@ -6,7 +6,7 @@ module.exports = {execute:
             console.log('error:', error); // Print the error if one occurred
             console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
             console.log('body:', body); // Print the HTML for the Google homepage.
-            Response.send(body, payload["Phone Number"]);
+            Response.send(body["description"], payload["Phone Number"]);
             });
     }
 }
