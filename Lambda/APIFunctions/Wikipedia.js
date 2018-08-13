86d30829924f93db1fb0188723cf542481a2837a
Ben Emmons <ben@emmons.biz>
2018-08-13 14:29:48 +0100

    Debugging

--- Lambda/APIFunctions/Wikipedia.js
+++ Lambda/APIFunctions/Wikipedia.js
@@ -6,6 +6,7 @@ module.exports = {execute:
             console.log('error:', error); // Print the error if one occurred
             console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
             console.log('body:', body); // Print the HTML for the Google homepage.
+            console.log(body["description"]);
             Response.send(body["description"], payload["Phone Number"]);
             });
     }
