cb896366dac81edefa8a701af4a4d125aa547457
Benji <benji@benjicraft.info>
2018-08-13 13:33:59 +0100

    Please to the lords above just work

--- Lambda/index.js
+++ Lambda/index.js
@@ -15,11 +15,7 @@ exports.handler = (event, context, callback) => {
     forwardQuery.sort(sms["body"]["intendedFunction"], sms["body"]["query"], sms["sender"])
     var response = {
         "statusCode": 200,
-        "headers": {
-            "my_header": "my_value"
-        },
-        "body": "",
         "isBase64Encoded": false
     };
-    callback();
+    callback(null, response);
 };
