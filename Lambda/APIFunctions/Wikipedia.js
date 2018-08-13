33152c9c3f223e7595e03d1d79aef58b9c49235d
Ben Emmons <ben@emmons.biz>
2018-08-13 14:20:41 +0100

    removed unnesseccary code

--- Lambda/APIFunctions/Wikipedia.js
+++ Lambda/APIFunctions/Wikipedia.js
@@ -2,7 +2,7 @@ const request = require('../node_modules/request');
 const Response = require("../Response")
 module.exports = {execute:
     function(payload) {
-        request('https://en.wikipedia.org/api/rest_v1/page/summary/' + wikipedia.page.data(payload["Parameters"]["Search Query"], function (error, response, body) {
+        request('https://en.wikipedia.org/api/rest_v1/page/summary/' + payload["Parameters"]["Search Query"], function (error, response, body) {
             console.log('error:', error); // Print the error if one occurred
             console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
             console.log('body:', body); // Print the HTML for the Google homepage.
