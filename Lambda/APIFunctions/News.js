98d68f03c997ea959fd6f942b8aef1dfb58d1e60
Benjamin Lewis <benji@benjisoft.org.uk>
2018-08-14 21:53:55 +0100

    News function

---
+++ Lambda/APIFunctions/News.js
@@ -0,0 +1,18 @@
+const request = require('../node_modules/request');
+const Response = require("../Response")
+module.exports = {
+    execute: function (payload) {
+        request('https://newsapi.org/v2/top-headlines?apiKey=c26b8ebdba81482e90d5b05d38336682', function (error, response, body) {
+            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+            console.log('body:', body); // Print the HTML for the Google homepage.
+            let description = JSON.parse(body)["articles"]["0"]["description"]
+            if (response.statusCode != 200) {
+                var errorStatus = true
+            } else {
+                var errorStatus = false
+            }
+            console.log(description)
+            Response.send(errorStatus, description, payload["Phone Number"]);
+        });
+    }
+}
