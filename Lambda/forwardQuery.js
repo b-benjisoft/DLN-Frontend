4ea3f422feacd4db5c36cd00b77d0e3115878ae7
Ben Emmons <ben@emmons.biz>
2018-08-15 09:34:53 +0100

    Fixed miscilanious

--- Lambda/forwardQuery.js
+++ Lambda/forwardQuery.js
@@ -30,38 +30,26 @@ module.exports = {
 
             case "news": {// Done Badly [Benji]
                 let payload = {"Phone Number": phoneNumber,
-                                "Parameters":{
-                                    "Country Code": query
-                                }
-            }
+                                "Parameters":{"Country Code": query}
+                            };
                 APIFunctions["News"].getNews(payload);
                 break;
             }
-                
-            case "dictionary": {//define? Yes // To do
-                break;}
-                
-            case "weather":{ // To Do
-                break;}
                 
             case "trains": { // To do [Benji]
                 let queryParameters = query.split(" to ");
                 let payload = {"Phone Number": phoneNumber,
                                "Parameters":{
                                 "Origin Station":        queryParameters[0],
-                                "Destination Station":   queryParameters[1]
-                            }}
+                                "Destination Station":   queryParameters[1]}
+                            };
 
                 APIFunctions["Trains"].execute(payload); 
                 break;
             }
                 
-            case "buses": {// To do
-                break;}
-                
             default:
                 return "Invalid Function"
-        }
-    
-    },
+        };
+    }
 };
