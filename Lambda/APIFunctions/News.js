76e9de135b86c240367f4659049f157e1717a2ca
Ben Emmons <ben@emmons.biz>
2018-08-16 00:17:09 +0100

    Changed news error catching

--- Lambda/APIFunctions/News.js
+++ Lambda/APIFunctions/News.js
@@ -9,11 +9,12 @@ module.exports = {
         request('https://newsapi.org/v2/top-headlines?apiKey=c26b8ebdba81482e90d5b05d38336682&country=' + ccode, function (error, response, body) {
             console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
             console.log('body:', body); // Print the HTML for the Google homepage.
-            let description = JSON.parse(body)["articles"]["0"]["description"]
-            if (response.statusCode != 200) {
-                var errorStatus = true
+            var description = "";
+            if (body.totalResults == 0) {
+                errorStatus = true;
             } else {
-                var errorStatus = false
+                errorStatus = false;
+                description = JSON.parse(body)["articles"]["0"]["description"];
             }
             console.log(description)
             Response.send(errorStatus, description, payload["Phone Number"]);
