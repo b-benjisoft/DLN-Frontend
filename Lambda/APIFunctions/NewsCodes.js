d71b1b8ba33f2690a329b0d15b166640ad6ce9b5
Ben Emmons <ben@emmons.biz>
2018-08-17 23:16:01 +0100

    Removed bracket

--- Lambda/APIFunctions/NewsCodes.js
+++ Lambda/APIFunctions/NewsCodes.js
@@ -1,6 +1,6 @@
 module.exports = {
 	getCountryCode: function (countryName) {
-		const isoCountries = require("./newsCodes.json"))
+		const isoCountries = require("./newsCodes.json")
 		console.log("isoCountries:", JSON.stringify(isoCountries));	
 		var code = isoCountries[countryName];
 		console.log(typeof isoCountries);
