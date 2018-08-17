3e53cd7f74287d84ba7271d3b700c6f8c458e0e3
Ben Emmons <ben@emmons.biz>
2018-08-17 22:55:49 +0100

    Debugging

--- Lambda/APIFunctions/NewsCodes.js
+++ Lambda/APIFunctions/NewsCodes.js
@@ -1,15 +1,17 @@
-const isoCountries = require("./countryCodes.json")
 module.exports = {
-	getCountryCode: function (countryName) {		
+	getCountryCode: function (countryName) {
+		const isoCountries = require("./countryCodes.json")
+		console.log("isoCountries:", JSON.stringify(isoCountries));	
 		var code = isoCountries[countryName];
-		console.log("NewsCodes Country", countryName)
-		console.log("NewsCodes Code", code)
+		console.log("NewsCodes Country", countryName);
+		console.log("NewsCodes Code", code);
 
 		if (code == undefined){
 			return("GB")
+		}else{
+			return(code);
 		}
-		return(code);
-		}
+	}
 
 
 }
