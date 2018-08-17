950e6bc5f38a8b776ee99298a8eed7a523302a11
Ben Emmons <ben@emmons.biz>
2018-08-17 23:13:39 +0100

    Debugging datatype of isoCountries

--- Lambda/APIFunctions/NewsCodes.js
+++ Lambda/APIFunctions/NewsCodes.js
@@ -1,8 +1,9 @@
 module.exports = {
 	getCountryCode: function (countryName) {
-		const isoCountries = JSON.parse(require("./newsCodes.json"))
+		const isoCountries = require("./newsCodes.json"))
 		console.log("isoCountries:", JSON.stringify(isoCountries));	
 		var code = isoCountries[countryName];
+		console.log(typeof isoCountries);
 		console.log("NewsCodes Country", countryName);
 		console.log("NewsCodes Code", code);
 
