ffda0ad9b16bf813d1be534548e8732583054c02
Benji <benji@benjicraft.info>
2018-08-31 23:21:32 +0100

    WIP

--- Lambda/APIFunctions/Trains.js
+++ Lambda/APIFunctions/Trains.js
@@ -1,4 +1,6 @@
 const codes = require("./stationCodes.json")
+const soapRequest = require('easy-soap-request');
+const fs = require('fs');
 
 function StationToCode(language){
     codes.forEach(function(code){
@@ -9,3 +11,20 @@ function StationToCode(language){
 }
 
 StationToCode("Bracknell")
+
+// example data
+const url = 'https://lite.realtime.nationalrail.co.uk/OpenLDBWS/wsdl.aspx?ver=2017-10-01';
+const headers = {
+  'user-agent': 'sampleTest',
+  'Content-Type': 'text/xml;charset=UTF-8',
+  'soapAction': 'https://graphical.weather.gov/xml/DWMLgen/wsdl/ndfdXML.wsdl#LatLonListZipCode',
+};
+const xml = fs.readFileSync('test/zipCodeEnvelope.xml', 'utf-8');
+
+// usage of module
+(async () => {
+  const { response } = await soapRequest(url, headers, xml);
+  const { body, statusCode } = response;
+  console.log(body);
+  console.log(statusCode);
+})();
